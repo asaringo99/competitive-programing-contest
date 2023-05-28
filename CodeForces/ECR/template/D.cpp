f466328ff986b8253d2c0fcd8e3cb40a6fa35233
asaringo <ka99code@gmail.com>
2023-05-28 16:01:50 +0900

    change template

--- CodeForces/ECR/template/D.cpp
+++ CodeForces/ECR/template/D.cpp
@@ -9,15 +9,14 @@ typedef tuple<ll,ll,ll> TP ;
 #define chmin(a,b) a = min(a,b)
 #define chmax(a,b) a = max(a,b)
 #define bit_count(x) __builtin_popcountll(x)
+#define leading_zero_count(x) __builtin_clz(x)
+#define trailing_zero_count(x) __builtin_ctz(x)
 #define gcd(a,b) __gcd(a,b)
 #define lcm(a,b) a / gcd(a,b) * b
 #define rep(i,n) for(int i = 0 ; i < n ; i++)
 #define rrep(i,a,b) for(int i = a ; i < b ; i++)
 #define repi(it,S) for(auto it = S.begin() ; it != S.end() ; it++)
 #define pt(a) cout << a << endl
-#define DEBUG(...) ; cout << #__VA_ARGS__ << endl ; for(auto x : {__VA_ARGS__}) cout << x << "  " ; cout << endl ;
-#define DEBUG_LIST(...) cout << #__VA_ARGS__ << endl ; DEBUG_REP(__VA_ARGS__) ;
-#define DEBUG_REP(V) cout << "{ " ; repi(itr,V) cout << *itr << ", " ; cout << "}" << endl ;
 #define debug(a) cout << #a << " " << a << endl
 #define all(a) a.begin(), a.end()
 #define endl "\n"
