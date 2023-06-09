b856da1061d0a9456487520babdbe2d2e53c7d0a
asaringo <ka99code@gmail.com>
2023-06-09 23:51:30 +0900

    AtCoder template

--- AtCoder/ABC/template/E.cpp
+++ AtCoder/ABC/template/E.cpp
@@ -1,10 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std ;
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr);
+#pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,fast-math")
 typedef long long ll ;
-#define MAX_N (ll)2e5
-#define YES cout << "Yes" << endl 
-#define NO cout << "No" << endl 
+typedef long double ld ;
+typedef pair<ll,ll> P ;
+typedef tuple<ll,ll,ll> TP ;
+#define chmin(a,b) a = min(a,b)
+#define chmax(a,b) a = max(a,b)
+#define bit_count(x) __builtin_popcountll(x)
+#define leading_zero_count(x) __builtin_clz(x)
+#define trailing_zero_count(x) __builtin_ctz(x)
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) a / gcd(a,b) * b
 #define rep(i,n) for(int i = 0 ; i < n ; i++)
-#define syosu setprecision(10)
-#define a_z "abcdefghijklmnopqrstuvwxyz"
-#define A_Z "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+#define rrep(i,a,b) for(int i = a ; i < b ; i++)
+#define repi(it,S) for(auto it = S.begin() ; it != S.end() ; it++)
+#define pt(a) cout << a << endl
+#define debug(a) cout << #a << " " << a << endl
+#define all(a) a.begin(), a.end()
+#define endl "\n"
+
+void solve(){
+    int n;
+    cin >> n;
+}
+
+int main(){
+    fast_io
+    // int t;
+    // cin >> t;
+    // rep(i,t) solve();
+    solve();
+}
