1fdff0846e35d7d387ff7115ea4b71ff7ba18004
asaringo <ka99code@gmail.com>
2023-06-18 19:04:02 +0900

    [fix] template

--- AtCoder/ABCex/template/template.cpp
+++ AtCoder/ABCex/template/template.cpp
@@ -4,24 +4,31 @@ using namespace std ;
 #pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,fast-math")
 typedef long long ll ;
 typedef long double ld ;
-typedef pair<ll,ll> P ;
-typedef tuple<ll,ll,ll> TP ;
 #define chmin(a,b) a = min(a,b)
 #define chmax(a,b) a = max(a,b)
 #define bit_count(x) __builtin_popcountll(x)
+#define leading_zero_count(x) __builtin_clz(x)
+#define trailing_zero_count(x) __builtin_ctz(x)
 #define gcd(a,b) __gcd(a,b)
 #define lcm(a,b) a / gcd(a,b) * b
 #define rep(i,n) for(int i = 0 ; i < n ; i++)
 #define rrep(i,a,b) for(int i = a ; i < b ; i++)
+#define repi(it,S) for(auto it = S.begin() ; it != S.end() ; it++)
 #define pt(a) cout << a << endl
+#define ptv(v) for(auto u : v) cout << u << " "; cout << endl
 #define debug(a) cout << #a << " " << a << endl
 #define all(a) a.begin(), a.end()
 #define endl "\n"
 
-int n;
+void solve(){
+    int n;
+    cin >> n;
+}
 
 int main(){
     fast_io
-    cin >> n;
-
+    // int t;
+    // cin >> t;
+    // rep(i,t) solve();
+    solve();
 }
