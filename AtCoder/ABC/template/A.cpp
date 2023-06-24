e5fd8c467c12d117094006cc06329d51bf7a7e53
asaringo <ka99code@gmail.com>
2023-06-24 20:46:14 +0900

    [update] template

--- AtCoder/ABC/template/A.cpp
+++ AtCoder/ABC/template/A.cpp
@@ -4,6 +4,8 @@ using namespace std ;
 #pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,fast-math")
 typedef long long ll ;
 typedef long double ld ;
+typedef pair<ll,ll> P ;
+typedef tuple<ll,ll,ll> TP ;
 #define chmin(a,b) a = min(a,b)
 #define chmax(a,b) a = max(a,b)
 #define bit_count(x) __builtin_popcountll(x)
@@ -14,15 +16,33 @@ typedef long double ld ;
 #define rep(i,n) for(int i = 0 ; i < n ; i++)
 #define rrep(i,a,b) for(int i = a ; i < b ; i++)
 #define repi(it,S) for(auto it = S.begin() ; it != S.end() ; it++)
-#define pt(a) cout << a << endl
-#define ptv(v) for(auto u : v) cout << u << " "; cout << endl
+#define pt(a) cout << a << endl;
 #define debug(a) cout << #a << " " << a << endl
 #define all(a) a.begin(), a.end()
 #define endl "\n"
+#define v1(n,a) vector<ll>(n,a)
+#define v2(n,m,a) vector<vector<ll>>(n,v1(m,a))
+#define v3(n,m,k,a) vector<vector<vector<ll>>>(n,v2(m,k,a))
+#define v4(n,m,k,l,a) vector<vector<vector<vector<ll>>>>(n,v3(m,k,l,a))
+template<typename T,typename U>istream &operator>>(istream&is,pair<T,U>&p){is>>p.first>>p.second;return is;}
+template<typename T,typename U>ostream &operator<<(ostream&os,const pair<T,U>&p){os<<"f: "<<p.first<<" s: "<<p.second;return os;}
+template<typename T>istream &operator>>(istream&is,vector<T>&v){for(T &in:v){is>>in;}return is;}
+template<typename T>ostream &operator<<(ostream&os,const vector<T>&v){for(auto it=v.begin();it!=v.end();){os<<*it<<((++it)!=v.end()?" ":"");}return os;}
+template<typename T>istream &operator>>(istream&is,vector<vector<T>>&v){for(T &in:v){is>>in;}return is;}
+template<typename T>ostream &operator<<(ostream&os,const vector<vector<T>>&v){for(auto it=v.begin();it!=v.end();){os<<*it<<((++it)!=v.end()?"\n":"");}return os;}
+template<typename T>ostream &operator<<(ostream&os,const set<T>&v){for(auto it=v.begin();it!=v.end();){os<<*it<<((++it)!=v.end()?" ":"");}return os;}
+template<typename T>ostream &operator<<(ostream&os,const multiset<T>&v){for(auto it=v.begin();it!=v.end();){os<<*it<<((++it)!=v.end()?" ":"");}return os;}
+template<typename T,typename U>ostream &operator<<(ostream&os,const map<T,U>&v){for(auto it=v.begin();it!=v.end();){os<<*it<<((++it)!=v.end()?"\n":"");}return os;}
+
+int n;
 
 void solve(){
-    int n;
     cin >> n;
+    vector<int> A(n);
+    cin >> A;
+    pt(A);
+    cin >> A;
+    pt(A);
 }
 
 int main(){
