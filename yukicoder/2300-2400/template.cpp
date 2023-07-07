495ea02f98034d4dba40af26797531555f1276a8
asaringo <ka99code@gmail.com>
2023-07-07 20:53:05 +0900

    template

--- yukicoder/2300-2400/template.cpp
+++ yukicoder/2300-2400/template.cpp
@@ -1,11 +1,11 @@
 #include <bits/stdc++.h>
-using namespace std ;
+using namespace std;
 #define fast_io ios::sync_with_stdio(false); cin.tie(nullptr);
 #pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,fast-math")
-typedef long long ll ;
-typedef long double ld ;
-#define chmin(a,b) a = min(a,b)
-#define chmax(a,b) a = max(a,b)
+typedef long long ll;
+typedef long double ld;
+#define chmin(a,b) a = min(a,b);
+#define chmax(a,b) a = max(a,b);
 #define bit_count(x) __builtin_popcountll(x)
 #define leading_zero_count(x) __builtin_clz(x)
 #define trailing_zero_count(x) __builtin_ctz(x)
@@ -14,10 +14,10 @@ typedef long double ld ;
 #define rep(i,n) for(int i = 0 ; i < n ; i++)
 #define rrep(i,a,b) for(int i = a ; i < b ; i++)
 #define repi(it,S) for(auto it = S.begin() ; it != S.end() ; it++)
-#define pt(a) cout << a << endl
-#define debug(a) cout << #a << " " << a << endl
+#define pt(a) cout << a << endl;
+#define debug(a) cout << #a << " " << a << endl;
 #define all(a) a.begin(), a.end()
-#define endl "\n"
+#define endl "\n";
 #define v1(n,a) vector<ll>(n,a)
 #define v2(n,m,a) vector<vector<ll>>(n,v1(m,a))
 #define v3(n,m,k,a) vector<vector<vector<ll>>>(n,v2(m,k,a))
@@ -29,6 +29,7 @@ template<typename T>ostream &operator<<(ostream&os,const vector<T>&v){for(auto i
 template<typename T>istream &operator>>(istream&is,vector<vector<T>>&v){for(T &in:v){is>>in;}return is;}
 template<typename T>ostream &operator<<(ostream&os,const vector<vector<T>>&v){for(auto it=v.begin();it!=v.end();){os<<*it<<((++it)!=v.end()?"\n":"");}return os;}
 template<typename T>ostream &operator<<(ostream&os,const set<T>&v){for(auto it=v.begin();it!=v.end();){os<<*it<<((++it)!=v.end()?" ":"");}return os;}
+template<typename T>ostream &operator<<(ostream&os,const multiset<T>&v){for(auto it=v.begin();it!=v.end();){os<<*it<<((++it)!=v.end()?" ":"");}return os;}
 
 void solve(){
     int n;
@@ -37,8 +38,7 @@ void solve(){
 
 int main(){
     fast_io
-    // int t;
+    int t = 1;
     // cin >> t;
-    // rep(i,t) solve();
-    solve();
+    rep(i,t) solve();
 }
